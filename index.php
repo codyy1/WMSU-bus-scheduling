455609ec71025948e2d888473c04d69e0e9e77f3
codyy1 <nyannyaww@gmail.com>
2025-10-27 15:34:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Admin Login - WMSU Transport</title>
-    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="../user/styles/styles.css">
 </head>
 <body>
 <header>
